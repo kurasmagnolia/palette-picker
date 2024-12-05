57d500bd9adecda86347d63774e7d93ffca53547
kurasmagnolia <kingmmcleod@outlook.com>
2024-12-05 15:35:44 -0500

    set up form submit handler + grabbed form data + fixed references using name attribute + fixed import name of starting palettes

--- app/src/main.js
+++ app/src/main.js
@@ -1,17 +1,38 @@
 import "./style.css";
 import { v4 as generateUUID } from "uuid";
-import palettes from "./starting-palettes.json";
-console.log(palettes);
+import startingPalettes from "./starting-palettes.json";
+// console.log(palettes);
 const newPaletteID = generateUUID();
 
-// Ordinarily, you'd add these elements to index.html
-const uuidButton = document.createElement("button");
-const uuidText = document.createElement("p");
-document.body.append(uuidButton, uuidText);
+// // Ordinarily, you'd add these elements to index.html
+// const uuidButton = document.createElement("button");
+// const uuidText = document.createElement("p");
+// document.body.append(uuidButton, uuidText);
 
-uuidButton.textContent = "Generate UUID";
+// uuidButton.textContent = "Generate UUID";
 
-uuidButton.addEventListener("click", () => {
-  const newUUID = generateUUID();
-  uuidText.textContent = `your new uuid is: ${newUUID}`;
-});
+// uuidButton.addEventListener("click", () => {
+//   const newUUID = generateUUID();
+//   uuidText.textContent = `your new uuid is: ${newUUID}`;
+// });
+
+const handleFormSubmit = (e) => {
+  // prevent the default action
+  e.preventDefault();
+
+  // grab the form data
+  const newPaletteTitle = e.target.title.value;
+  const newPaletteColorOne = e.target.color1.value;
+  const newPaletteColorTwo = e.target.color2.value;
+  const newPaletteColorThree = e.target.color3.value;
+  const newPaletteWarmTemp = e.target.warmTemp.value;
+  const newPaletteNeutralTemp = e.target.neutralTemp.value;
+  const newPaletteCoolTemp = e.target.coolTemp.value;
+};
+
+const main = () => {
+  // attach the form event handler to listen for the submit event
+  document.querySelector("form").addEventListener("submit", handleFormSubmit);
+};
+
+main();
