3fc6c034f442a89aca447d3f1ae3b40b8bf18f90
kurasmagnolia <kingmmcleod@outlook.com>
2024-12-07 06:27:40 -0500

    fixed main.js due to misunderstanding

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,11 @@
 import "./style.css";
-import { getPalettes, initializePalettes } from "./local-storage";
+import {
+  getPalettes,
+  initializePalettes,
+  addPalette,
+  deletePaletteByID,
+} from "./local-storage.js";
+import { showPalettes, addPaletteToList } from "./dom-helpers.js";
 
 const handleFormSubmit = (e) => {
   // prevent the default action
@@ -12,9 +18,6 @@ const handleFormSubmit = (e) => {
   const newPaletteColorThree = e.target.color3.value;
   const newPaletteTemp = e.target.temp.value;
 
-  // store palettes
-  const palettes = {};
-
   // create new palette
   const newPalette = {
     uuid: crypto.randomUUID(),
@@ -22,8 +25,13 @@ const handleFormSubmit = (e) => {
     colors: [newPaletteColorOne, newPaletteColorTwo, newPaletteColorThree],
     temperature: newPaletteTemp,
   };
-  palettes[newPalette.uuid] = newPalette;
-  console.log(palettes);
+
+  // stores palette in localStorage
+  addPalette(newPalette);
+
+  // stores the palette in the Palettes List
+  addPaletteToList(newPalette);
+
   // reset the form
   e.target.reset();
 };
