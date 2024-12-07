40d9685b35c186af72fa60929820c3a6bd245720
kurasmagnolia <kingmmcleod@outlook.com>
2024-12-07 05:09:07 -0500

    removed unnecessary code from main.js and added renderPalettes function to main function

--- app/src/main.js
+++ app/src/main.js
@@ -1,21 +1,6 @@
 import "./style.css";
-import { v4 as generateUUID } from "uuid";
-import startingPalettes from "./starting-palettes.json";
 import { getPalettes, initializePalettes } from "./local-storage";
-// console.log(palettes);
-const newPaletteID = generateUUID();
-
-// // Ordinarily, you'd add these elements to index.html
-// const uuidButton = document.createElement("button");
-// const uuidText = document.createElement("p");
-// document.body.append(uuidButton, uuidText);
-
-// uuidButton.textContent = "Generate UUID";
-
-// uuidButton.addEventListener("click", () => {
-//   const newUUID = generateUUID();
-//   uuidText.textContent = `your new uuid is: ${newUUID}`;
-// });
+const newPaletteID = crypto.randomUUID();
 
 const handleFormSubmit = (e) => {
   // prevent the default action
@@ -39,6 +24,8 @@ const main = () => {
   if (!getPalettes()) {
     initializePalettes();
   }
+  // render the exisitig palettes
+  renderPalettes();
   // attach the form event handler to listen for the submit event
   document.querySelector("form").addEventListener("submit", handleFormSubmit);
 };
