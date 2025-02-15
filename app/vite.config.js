e465c6e2fe9732c1f1fd5d6ed7dda8f13ff76091
kurasmagnolia <kingmmcleod@outlook.com>
2025-02-15 12:56:38 -0500

    deployment fix #3

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,6 @@
 /** @format */
 
 import { defineConfig } from 'vite';
-
 export default defineConfig({
   // GitHub Pages expects an index.html in the root directory
   // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
