eb2f86d919160e9b850874607a2e4c2ed6a9c4b6
kurasmagnolia <kingmmcleod@outlook.com>
2025-02-15 00:54:16 -0500

    deployment fix #2

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // GitHub Pages expects an index.html in the root directory
   // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-  build: { outDir: './dist' },
+  build: { outDir: '..' },
   // needed for github pages just put the repo name here
   base: '/palette-picker/',
 });
